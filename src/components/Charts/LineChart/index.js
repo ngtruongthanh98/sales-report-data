589c37f1895ad5399f45118bd217803adb59dd6d
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2022-11-21 20:43:44 +0700

    feat: Get data from props

--- src/components/Charts/LineChart/index.js
+++ src/components/Charts/LineChart/index.js
@@ -14,7 +14,7 @@ import faker from 'faker';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-const LineChart = () => {
+const LineChart = (props) => {
   const options = {
     responsive: true,
     plugins: {
@@ -28,33 +28,17 @@ const LineChart = () => {
     },
   };
 
-  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
   const data = {
-    labels: [
-      {
-        label: 'Dataset 1',
-        data: [""]
-      },
-      {
-        label: 'Dataset 2',
-        data: [""]
-      },
-    ],
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
+    labels: props.labelArray,
+    datasets: props.dataArray.map((data, index) => {
+      return {
+        label: props.labelArray[index],
+        // Mock data
+        data: props.labelArray.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: props.borderColorArray[index],
+        backgroundColor: props.backgroundColorArray[index],
+      };
+    }),
   };
 
   return <Line options={options} data={data} />;
@@ -63,6 +47,22 @@ const LineChart = () => {
 LineChart.defaultProps = {
   dataArray: [12, 19, 3, 5, 2, 3],
   labelArray: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  borderColorArray: [
+    'rgb(255, 99, 132)',
+    'rgb(53, 162, 235)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(153, 102, 255)',
+    'rgb(255, 159, 64)',
+  ],
+  backgroundColorArray: [
+    'rgba(255, 99, 132, 0.5)',
+    'rgba(53, 162, 235, 0.5)',
+    'rgba(255, 205, 86, 0.5)',
+    'rgba(75, 192, 192, 0.5)',
+    'rgba(153, 102, 255, 0.5)',
+    'rgba(255, 159, 64, 0.5)',
+  ],
 };
 
 export default LineChart;
