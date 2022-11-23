d2f63388a3256fed31158ccf956f707b6994df10
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-11-23 18:57:41 +0700

    feat: Async select input component

--- src/pages/Sales/index.js
+++ src/pages/Sales/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import './styles.scss';
+import DebounceSelect from '../../components/DebounceSelect';
 
 const Sales = () => {
-  return <div>Sale Person</div>;
+  const [value, setValue] = React.useState([]);
+
+  async function fetchUserList(username) {
+    console.log('fetching user', username);
+    return fetch('https://randomuser.me/api/?results=5')
+      .then((response) => response.json())
+      .then((body) =>
+        body.results.map((user) => ({
+          label: `${user.name.first} ${user.name.last}`,
+          value: user.login.username,
+        }))
+      );
+  }
+
+  return (
+    <div className="sale-page">
+      <div className="sale-page__body">
+        <h1 className="title">Sale person</h1>
+        <DebounceSelect
+          mode="multiple"
+          value={value}
+          placeholder="Select users"
+          fetchOptions={fetchUserList}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          style={{
+            width: '100%',
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Sales;
